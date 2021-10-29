23891900ce30c89e6fcd021f0dfbd723877ff7d4
Bob Grim <grimb300@hotmail.com>
2021-10-29 11:32:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blank Divi Child Theme
+
+Based on the Elegant Themes [tutorial](https://www.elegantthemes.com/blog/divi-resources/divi-child-theme).
---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function my_theme_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
